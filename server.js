592dc31e11a074cc7f4aeb307cd0fcfec66d74a2
Luca Urbanowicz <human.exp11@gmail.com>
2022-03-18 10:00:28 -0600

    added intake info and switch case in server.js

--- server.js
+++ server.js
@@ -22,9 +22,57 @@ const db = mysql.createConnection(
       database: 'tracker_db'
     },
     console.log(`Connected to the tracker_db database.`)
+  
 );
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    promptIntake();
 });
   
+function promptIntake() {
+  inquirer
+    .prompt({
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Departments",
+        "View All Roles",
+        "View All Employees",
+        "Add Department",
+        "Add Role",
+        "Add Employee",
+        "Update Employee Role",
+        "Quit"
+      ],
+    })
+    .then(function(result) {
+      console.log("Options for: " + result.option);
+
+      switch (result.option) {
+        case "View All Departments":
+          viewDepartment();
+          break;
+          case "View All Roles":
+            viewRoles();
+            break;
+            case "View All Employees":
+              viewEmployees();
+              break;
+        case "Add a Department":
+          addDepartment();
+          break;
+        case "Add a Role":
+          addRole();
+          break;
+        case "Add an Employee":
+          addEmployee();
+          break;
+        case "Update an Employee Role":
+          updateEmployee();
+          break;
+        default:
+          quit();
+      }
+    });
+}
